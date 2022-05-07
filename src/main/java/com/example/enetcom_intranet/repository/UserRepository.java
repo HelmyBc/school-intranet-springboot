87f3986b78a0d834ce8b31bbed05088d28c7d0f6
HelmyBc <helmibouchiha@gmail.com>
2022-05-08 00:42:44 +0100

    version 4.0 beginning the auth

--- src/main/java/com/example/enetcom_intranet/repository/UserRepository.java
+++ src/main/java/com/example/enetcom_intranet/repository/UserRepository.java
@@ -4,4 +4,5 @@ import com.example.enetcom_intranet.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User,Integer> {
+    User findByEmailAndPassword(String email,String password);
 }
